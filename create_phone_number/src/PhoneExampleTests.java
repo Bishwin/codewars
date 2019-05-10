806f9033fe374d512de574cf0c0a37553ee7a347
Jake <jake.huggins@solirius.com>
2019-05-10 20:25:26 +0100

    add new test case for input size

--- create_phone_number/src/PhoneExampleTests.java
+++ create_phone_number/src/PhoneExampleTests.java
@@ -9,4 +9,10 @@ public class PhoneExampleTests {
                 Kata.createPhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}));
     }
 
+    @Test
+    public void inputArraySizeMustBe10() {
+        assertEquals("input wrong size",
+                Kata.createPhoneNumber(new int[] {7, 8, 9}));
+    }
+
 }
